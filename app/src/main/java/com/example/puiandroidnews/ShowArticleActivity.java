49e8bb5196fa6013ed894213d44cdc43ffdbcdd5
jakuborlinski <jakub.orlinski.upm@gmail.com>
2021-11-29 14:30:02 +0100

    Add the article view activity

---
+++ app/src/main/java/com/example/puiandroidnews/ShowArticleActivity.java
@@ -0,0 +1,14 @@
+package com.example.puiandroidnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_article);
+    }
+}

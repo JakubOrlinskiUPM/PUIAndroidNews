5c36f62d4bcc2713e1a06e3b55b63ab5641d6d59
Avrodos <sajjad.ahmad@student.kit.edu>
2021-12-07 10:21:42 +0100

    Login functionality now uses remote authentication.

--- app/src/main/java/com/example/puiandroidnews/LoginActivity.java
+++ app/src/main/java/com/example/puiandroidnews/LoginActivity.java
@@ -8,6 +8,10 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.puiandroidnews.exceptions.AuthenticationError;
+
+import java.util.Properties;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
@@ -22,15 +26,34 @@ public class LoginActivity extends AppCompatActivity {
         // check if correct credentials
         String username = usernameField.getText().toString();
         String password = passwordField.getText().toString();
-        if (username.equals("user") && password.equals("password")) {
-            MainActivity.loggedIn = true;
-            System.out.println("Successfully logged in.");
-            finish();
-        } else {
-            // add toast
-            Toast.makeText(LoginActivity.this, "Invalid credentials!",
-                    Toast.LENGTH_SHORT).show();
-        }
+        Properties props = new Properties();
+        props.setProperty(ModelManager.ATTR_SERVICE_URL, "https://sanger.dia.fi.upm.es/pmd-task/");
+        props.setProperty(ModelManager.ATTR_LOGIN_USER, username);
+        props.setProperty(ModelManager.ATTR_LOGIN_PASS, password);
+
+        // Make sure we connect in the background and not in the main thread
+        Thread thread = new Thread(() -> {
+            try {
+                ModelManager modelManager = new ModelManager(props);
+            } catch (AuthenticationError authenticationError) {
+                authenticationError.printStackTrace();
+                // add toast
+                Toast.makeText(LoginActivity.this, "Invalid credentials!",
+                        Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        thread.start();
+
+
+        // successful login, since no authentication error
+        MainActivity.loggedIn = true;
+        System.out.println("Successfully logged in.");
+        finish();
+
     }
 
     //TODO: add remember me functionality and use remote authentication
